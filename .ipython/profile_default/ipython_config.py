7fb2156aa9604e95b56945f36bc2f7b575559de0
Aaron Maurais <aaron@Aarons-2024-MBP.local>
2025-02-24 13:30:09 -0800

    Add ipython config.

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+## lines of code to run at IPython startup.
+#  Default: []
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+
+## A list of dotted module names of IPython extensions to load.
+#  Default: []
+c.InteractiveShellApp.extensions = ['autoreload']
